4d0684ed1ac84d1139a58144e005e45cf63642b0
Andrii Salogub <andrejsalogub3@gmail.com>
2022-08-02 11:34:17 +0300

    add animation to welcome element with framer motion

--- src/Components/Welcome/Welcome.tsx
+++ src/Components/Welcome/Welcome.tsx
@@ -8,25 +8,62 @@ interface WelcomeProp {
 
 const Welcome = ({ OpenPopUp }: WelcomeProp) => {
     return (
-        <div className="welcome">
+        <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1 }}
+            className="welcome"
+        >
             <div className="welcome-img">
-                <div className="welcome-icon">WELCOME TO</div>
-                <h2 className="welcome-title">
+                <motion.div
+                    initial={{ y: 100, x: -10, opacity: 0 }}
+                    animate={{ y: 40, x: -10, opacity: 1 }}
+                    transition={{ delay: 1, duration: 0.5 }}
+                    className="welcome-icon"
+                >
+                    WELCOME TO
+                </motion.div>
+                <motion.h2
+                    initial={{ y: 100, x: -30, opacity: 0 }}
+                    animate={{ y: -50, x: -30, opacity: 1 }}
+                    transition={{ delay: 1.1, duration: 0.5 }}
+                    className="welcome-title"
+                >
                     <span className="welcome-shop-online">online shop</span>{' '}
                     with car parts{' '}
-                </h2>
-                <h3 className="welcome-subtitle">
+                </motion.h2>
+                <motion.h3
+                    initial={{ y: 100, x: -25, opacity: 0 }}
+                    animate={{ y: -70, x: -25, opacity: 1 }}
+                    transition={{ delay: 1.2, duration: 0.5 }}
+                    className="welcome-subtitle"
+                >
                     Lorem ipsum dolor laoreet enim. Etiam ullamcorper.
                     Suspendisse a pellentesque dui laoreet enim.
-                </h3>
-                <button className="welcome-btn">SEE OUR PRODUCTS &#62;</button>
-                <div className="welcome-popup-video-img"></div>
-                <button
+                </motion.h3>
+                <motion.button
+                    initial={{ y: -50, opacity: 0 }}
+                    animate={{ y: -100, opacity: 1 }}
+                    transition={{ delay: 1.3, duration: 0.5 }}
+                    className="welcome-btn"
+                >
+                    SEE OUR PRODUCTS &#62;
+                </motion.button>
+                <motion.div
+                    initial={{ x: -100, opacity: 0 }}
+                    animate={{ x: 0, opacity: 1 }}
+                    transition={{ delay: 1 }}
+                    className="welcome-popup-video-img"
+                ></motion.div>
+                <motion.button
+                    initial={{ x: -100, opacity: 0 }}
+                    animate={{ x: 0, opacity: 1 }}
+                    transition={{ delay: 1 }}
                     className="welcome-popup-video-btn"
                     onClick={() => OpenPopUp()}
-                ></button>
+                ></motion.button>
             </div>
-        </div>
+        </motion.div>
     )
 }
 
