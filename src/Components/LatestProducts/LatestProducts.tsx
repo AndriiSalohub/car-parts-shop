b0f54331fe95507679c0e016300766e164884292
Andrii Salogub <aominedaikivs@gmail.com>
2022-08-08 18:56:04 +0300

    fix bags and rewrite latest products functionality

--- src/Components/LatestProducts/LatestProducts.tsx
+++ src/Components/LatestProducts/LatestProducts.tsx
@@ -1,16 +1,44 @@
-import React from 'react'
+import React, { FC } from 'react'
 import { useAppSelector } from '../../Hooks/hooks'
 import ProductsSection from '../ProductsSection/ProductsSection'
 import './LatestProducts.scss'
 
-const LatestProducts = () => {
-    const latestProducts = useAppSelector((state) => state.parts.parts)
+const LatestProducts: FC = () => {
+    const allProducts: Array<{
+        id: number
+        title: string
+        price: number
+        discount: boolean
+        discountPrice: number
+        productCode: string
+        manufacturer: string
+        image: string
+    }> = useAppSelector((state) => state.parts.parts)
+
+    const latestProducts: Array<{
+        id: number
+        title: string
+        price: number
+        discount: boolean
+        discountPrice: number
+        productCode: string
+        manufacturer: string
+        image: string
+    }> = []
+
+    for (let i = 0; i < allProducts.length - 1; i++) {
+        for (let j = 1; j <= 4; j++) {
+            if (allProducts[i].id === allProducts.length - j) {
+                latestProducts.push(allProducts[i])
+            }
+        }
+    }
 
     return (
         <section className="latest-products">
             <ProductsSection
                 sectionTitle="latest products"
-                products={latestProducts.slice(-4)}
+                products={latestProducts}
             />
         </section>
     )
