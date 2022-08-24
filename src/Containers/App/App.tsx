433695bd2d49455c4abd1c7ed97c0ca4d5a8fd56
Andrii Salogub <aominedaikivs@gmail.com>
2022-08-24 11:29:00 +0300

    add functionality when user click on buy more

--- src/Containers/App/App.tsx
+++ src/Containers/App/App.tsx
@@ -2,18 +2,19 @@ import React, { FC, useEffect } from 'react'
 import { Routes, Route, useLocation } from 'react-router-dom'
 import { useAppDispatch } from '../../Hooks/hooks'
 import { getParts } from '../../ReduxToolkit/Slices/PartsSlice/PartsSlice'
+import { AnimatePresence } from 'framer-motion'
 import AboutPage from '../../Pages/AboutPage'
 import ContactPage from '../../Pages/ContactPage'
 import HomePage from '../../Pages/HomePage'
 import CategoriesPage from '../../Pages/CategoriesPage'
 import ShopPage from '../../Pages/ShopPage'
-import { AnimatePresence } from 'framer-motion'
 
 const App: FC = () => {
-    const dispatch = useAppDispatch()
+    const dispatch: Function = useAppDispatch()
 
     useEffect(() => {
         dispatch(getParts())
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
     const location = useLocation()
