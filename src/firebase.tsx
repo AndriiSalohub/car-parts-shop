22484718500426c1a7972b15afdf3565114cf23e
Andrii <andrejsalogub3@gmail.com>
2023-08-14 13:35:55 +0300

    added changes to firebase

--- src/firebase.tsx
+++ src/firebase.tsx
@@ -1,14 +1,14 @@
-import { initializeApp } from 'firebase/app'
 import { getFirestore } from '@firebase/firestore'
+import { initializeApp } from 'firebase/app'
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_DATABASE_URL,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    apiKey: 'AIzaSyAmJ5dn52bpvywPuiafAMKW1QNHrAeoC24',
+    authDomain: 'car-parts-shop-751a4.firebaseapp.com',
+    databaseURL: 'https://car-parts-shop-751a4-default-rtdb.firebaseio.com',
+    projectId: 'car-parts-shop-751a4',
+    storageBucket: 'car-parts-shop-751a4.appspot.com',
+    messagingSenderId: '204188304408',
+    appId: '1:204188304408:web:1bdec8433af98b329c2f6d',
 }
 
 const app = initializeApp(firebaseConfig)
